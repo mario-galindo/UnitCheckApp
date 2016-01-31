c545bedfe06948dbc13f356b840b40176ddecf77
Mario Galindo <marioga_lindo@hotmail.es>
2016-01-31 17:08:40 -0600

    commit inicial

---
+++ js/app.js
@@ -0,0 +1,50 @@
+var app = angular.module('UnitCheckApp', [
+
+		'ngRoute'
+	])
+
+		.config(function($routeProvider) {
+			
+			$routeProvider
+				.when('/',{
+					
+					templateUrl:'views/login.html',
+					controller:'loginControl'
+				})
+
+				.when('/main',{
+
+					resolve:{
+
+						"check":function($location,$rootScope){
+
+							if(!$rootScope.logeado){
+								$location.path('/')
+							}
+						}
+					},
+						templateUrl:'views/main.html'
+				})
+
+				.when('/consulta',{
+
+					resolve:{
+
+						"ckeck":function($location,$rootScope){
+
+							if(!$rootScope.logeado){
+								$location.path('/')
+							}
+						}
+					},
+
+					templateUrl:'views/consulta.html'
+				})
+
+				
+				.otherwise({
+
+					redirectTo:'/'
+				});
+
+		});
---
+++ js/controllers/login-ctrl.js
@@ -0,0 +1,49 @@
+app.controller('loginControl',function($scope,$http,$location,$rootScope){
+
+	
+	
+		//Parametros
+		$scope.usuario = "";
+		$scope.password = ""; 
+
+
+
+		
+		//Request
+		$scope.funcion_login = function(){
+			
+			//Variable con los Parametros
+			var data = {
+				username: $scope.usuario,
+				password:$scope.password
+			};
+
+
+			$http({
+	 			 method: 'POST',
+	  			 url: 'http://rastreo1.sabuesotrack.com:3000/login'
+
+			//Cuando el request sea exitoso nos devuelve la informacion en la variable data
+			}).then(function(data) {
+    			
+    			console.log(data);
+
+    			if($scope.usuario == 'mario' && $scope.password == '123'){
+    				
+    				$rootScope.logeado = true;
+
+    				$location.path('/main');
+    				console.log("Si Existe!!");
+    			}else{
+    				console.log("No Existe");
+    			}
+
+    		//Cuando hay un error en el request nos devuelve el error en la variable error
+  			}, function(error) {
+    			
+    			console.log(error);
+  			});
+			
+		};
+		
+});
